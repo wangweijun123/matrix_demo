2903ca77bc6af2e6b37290e7b09aa02bb67b5186
Your Name <you@example.com>
2021-09-22 22:20:57 +0800

    'init'

---
+++ app/src/main/java/com/dodola/matrixdemo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.dodola.matrixdemo;
+
+import android.app.Application;
+
+import com.tencent.matrix.Matrix;
+import com.tencent.matrix.iocanary.IOCanaryPlugin;
+import com.tencent.matrix.iocanary.config.IOConfig;
+
+public class DemoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Matrix.Builder builder = new Matrix.Builder(this); // build matrix
+        builder.pluginListener(new TestPluginListener(this)); // add general pluginListener
+        DynamicConfigImplDemo dynamicConfig = new DynamicConfigImplDemo(); // dynamic config
+
+        // init plugin
+        IOCanaryPlugin ioCanaryPlugin = new IOCanaryPlugin(new IOConfig.Builder()
+                .dynamicConfig(dynamicConfig)
+                .build());
+        //add to matrix
+        builder.plugin(ioCanaryPlugin);
+
+        //init matrix
+        Matrix.init(builder.build());
+
+        // start plugin
+        ioCanaryPlugin.start();
+
+    }
+}
